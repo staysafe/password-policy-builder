f63fdfdb53b8f55bf0601541f36207daca44f5b9
Mazvita Selemani <mazvita.selemani@ecoonline.com>
2023-11-20 16:55:29 +0000

    [SS4-3353] Add Array Policy to Policy directory

---
+++ src/Policy/ArrayPolicy.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\User\Password\Policy;
+
+use StaySafe\Password\Policy\Policy\PolicyInterface;
+use StaySafe\Password\Policy\Rule\RuleInterface;
+use StaySafe\Password\Policy\Rule\Exception\InvalidRuleTypeException;
+use StaySafe\Password\Policy\Rule\Exception\InvalidConstraintException;
+
+final class ArrayPolicy implements PolicyInterface
+{
+    /** @var RuleInterface[] */
+    private array $constraints = [];
+
+    /**
+     * @throws InvalidConstraintException
+     * @throws InvalidRuleTypeException
+     */
+    public function __construct(array $policy)
+    {
+        $this->loadConstraints($policy);
+    }
+
+    /**
+     * @throws InvalidConstraintException
+     * @throws InvalidRuleTypeException
+     */
+    private function loadConstraints(array $policy): void
+    {
+        if (empty($policy)) {
+            throw new InvalidConstraintException('Policy cannot be empty');
+        }
+
+        foreach ($policy as $ruleClassName => $number) {
+            $this->constraints[$ruleClassName] = $this->getRule($ruleClassName, $number);
+        }
+    }
+
+    /**
+     * @throws InvalidRuleTypeException
+     */
+    private function getRule(string $ruleClassName, int $number): RuleInterface
+    {
+        if (!is_subclass_of($ruleClassName, RuleInterface::class)) {
+            throw new InvalidRuleTypeException(sprintf('Rule class %s is not valid', $ruleClassName));
+        }
+
+        return new $ruleClassName($number);
+    }
+
+    /**
+     * @return RuleInterface[]
+     */
+    public function getConstraints(): array
+    {
+        return $this->constraints;
+    }
+}
