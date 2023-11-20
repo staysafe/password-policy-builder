0d57ac49c2f25a3cd4f25248a0eddeb4131622fb
Mazvita Selemani <mazvita.selemani@ecoonline.com>
2023-11-20 14:07:52 +0000

    [SS4-3353] Test PasswordPolicyBuilder constructor and existing methods

---
+++ tests/phpunit/PasswordPolicyBuilderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use StaySafe\Password\Policy\PasswordPolicyBuilder;
+use StaySafe\Password\Policy\Policy\JsonPolicy;
+
+final class PasswordPolicyBuilderTest extends TestCase {
+
+    function test_password_policy_object_contains_password_policy(){
+
+        $jsonConstraints = file_get_contents(__DIR__ . '/fixtures/policy.json');
+        $policy = new JsonPolicy($jsonConstraints);
+
+        $passwordPolicyBuilder = new PasswordPolicyBuilder($policy);
+
+        $actualPolicy = $passwordPolicyBuilder->getPolicy();
+
+        self::assertEquals($policy, $actualPolicy);
+
+    }
+
+    function test_password_that_meets_constraints_is_valid(){
+
+        $jsonConstraints = file_get_contents(__DIR__ . '/fixtures/policy.json');
+        $policy = new JsonPolicy($jsonConstraints);
+
+        $passwordPolicyBuilder = new PasswordPolicyBuilder($policy);
+
+        $validConstraint = $passwordPolicyBuilder->isValid('Pho$4r0us!12');
+
+        self::assertTrue($validConstraint);
+
+    }
+
+
+
+}
