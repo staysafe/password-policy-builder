d4bf59b19e0a432d73b2ff73caf0db96ff0249dc
Mazvita Selemani <mazvita.selemani@ecoonline.com>
2023-11-20 17:13:04 +0000

    [SS4-3353] Migrate password policy builder from hub to package

--- src/PasswordPolicyBuilder.php
+++ src/PasswordPolicyBuilder.php
@@ -41,4 +41,66 @@ class PasswordPolicyBuilder implements PasswordPolicyBuilderInterface
     {
         return $this->policy;
     }
+
+    /**
+     * @param RuleInterface[] $enforcedRules
+     */
+    public static function createWithEnforcedRules(PolicyInterface $policy, array $enforcedRules = []): self
+    {
+        return new self(
+            new PasswordPolicyBuilder(
+                self::createPolicy($policy, $enforcedRules)
+            )
+        );
+    }
+
+    /**
+     * @param RuleInterface[] $enforcedRules
+     */
+    private static function createPolicy(PolicyInterface $policy, array $enforcedRules): PolicyInterface
+    {
+        $rules = self::getRules($policy->getConstraints());
+        $rules += self::getRules($enforcedRules);
+
+        return new ArrayPolicy($rules);
+    }
+
+    /**
+     * @param RuleInterface[] $constraints
+     *
+     * @return array<class-string, int>
+     */
+    private static function getRules(array $constraints): array
+    {
+        return self::flattenRules(self::getRulesFromConstraints($constraints));
+    }
+
+    /**
+     * @param array<int, array<class-string, int>> $rules
+     *
+     * @return array<class-string, int>
+     */
+    private static function flattenRules(array $rules): array
+    {
+        return array_merge(...$rules);
+    }
+
+    /**
+     * @param RuleInterface[] $constraints
+     *
+     * @return array<int, array<class-string, int>>
+     */
+    private static function getRulesFromConstraints(array $constraints): array
+    {
+        return array_map(self::getRuleFromConstraint(...), $constraints, []);
+    }
+
+    /**
+     * @return array<class-string, int>
+     */
+    private static function getRuleFromConstraint(RuleInterface $constraint): array
+    {
+        return $constraint->getRule();
+    }
+
 }
